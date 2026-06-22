35a3d7793c71c47588d9e627129acc7ec4afae98
Nael Ibrahim <naelbaban2@gmail.com>
2026-06-22 10:31:00 +0700

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "data_lake" {
+  bucket = "my-data-lake-${var.environment}"
+}
+
+resource "aws_s3_bucket_versioning" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+  
+  rule {
+    id     = "transition-to-glacier"
+    status = "Enabled"
+    
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+    
+    expiration {
+      days = 365
+    }
+  }
+}
